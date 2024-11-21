c053c85c01147fbf76ab57933f222d1d0029c746
Jie Sen Huang <71396823+senjieh@users.noreply.github.com>
2024-11-20 23:12:28 -0800

    add: design and preliminary changes

--- app/page.js
+++ app/page.js
@@ -1,4 +1,4 @@
-"use client"
+
 import { useState, useEffect } from 'react';
 import Image from "next/image";
 import LottieAnimation from './components/LottieAnimation';
@@ -8,9 +8,25 @@ import AnimatedText from './components/AnimatedText';
 import AnimatedWho from "./components/AnimationWho";
 import Link from 'next/link'
 
-export default function Home() {
+import axios from 'axios';
+
+export default async function Home() {
   const [isLoaded, setIsLoaded] = useState(false);
   const [showTextAnimation, setShowTextAnimation] = useState(false);
+
+  const fetchInstagramFeed = async () => {
+    const token = '';
+    const url = `https://graph.instagram.com/me/media?fields=id,caption,media_type,media_url,thumbnail_url,permalink&access_token=${token}`;
+    
+    try {
+      const response = await axios.get(url);
+      return response.data.data;
+    } catch (error) {
+      console.error("Error fetching Instagram feed:", error);
+      return [];
+    }
+  };
+
 
   useEffect(() => {
     // Assuming you want to wait for images to load as well
@@ -51,62 +67,59 @@ export default function Home() {
     }
   }, [isLoaded]);
 
+  const test = await fetchInstagramFeed()
+  console.log(test);
+
   return (
     <div>
       {isLoaded && (
         <main className="flex min-h-screen w-full flex-col items-center">
-          <nav className="fixed flex z-50"> 
+          <nav className="fixed flex z-50 -mb-20"> 
             <div className="mx-10">
-              <Link href="/">Home</Link>
+              <Link href="/">HOME</Link>
             </div>
             <div className="mx-10">
-              <Link href="/about">About</Link>
+              <Link href="/about">ABOUT</Link>
             </div>
             <div className="mx-10">
-              <Link href="/contact">Contact</Link>
+              <Link href="/about">SOCIALS</Link>
+            </div>
+            <div className="mx-10">
+              <Link href="/contact">CONTACT</Link>
             </div>
           </nav>
-          <div className='flex flex-col items-center justify-center h-screen'>
+          <div className='absolute'>
             <div className="fixed items-center justify-center object-left-top top-0 left-0 bottom-0 right-0 flex z-40">
               <LottieAnimation animationData={introAnimationData} />
             </div>
-            <div className='flex items-center justify-center content-center justify-items-center z-10'>
-              
-              {showTextAnimation && (
-                <div className='flex flex-col content-center'>
-                  <div className='mb-10'>
-                    <button className='animatedButton'>Hello! My name is</button>
-                  </div>
-          
-                  <AnimatedText text="JIE SEN"/>
-                  <AnimatedText text="HUANG"/>
-                  <div className="mt-20">
-                    <AnimatedWho
-                      precursorText="I am a "
-                      texts={["Developer", "Designer", "Editor", "Entrepreneur"]}
-                      fontSize="2vw"
-                    />
-                  </div>
-                </div>
-              )}
-              <div className='main-portfolio-image'>
-                <Image
-                  src="/images/portpic.png"
-                  width={500}
-                  height={500}
-                  alt="Picture of the author"
-                />
+          </div>
+          <div className='h-screen w-2/3'>
+            <div></div>
+            <div className='flex flex-row h-1/2 items-end justify-between'>
+              <h1 >jiesen</h1>
+              <h1 >huang.</h1>
+            </div>
+            <div className='h-1/2'>
+              <div className='h-1/3'>
+                <div></div>
+                <div></div>
+              </div>
+              <div className='h-1/3'>
+                <div></div>
+                <div></div>
+              </div>
+              <div className='h-1/3'>
+                <div></div>
+                <div></div>
               </div>
             </div>
-          </div>
-          <div className="fixed items-center justify-center object-left-top top-0 left-0 bottom-0 right-0 flex z-0">
-            <LottieAnimation animationData={griddyAnimationData} delay={1600} />
+
           </div>
         </main>
       )}
       {!isLoaded && (
-        <main className="flex min-h-screen w-full flex-col items-center justify-center" style={{backgroundColor: '#07111a'}}>
-          <h1 style={{color: '#ffffff', fontFamily: 'SEQUEL100'}}>Loading...</h1>
+        <main className="flex min-h-screen w-full flex-col items-center justify-center" style={{backgroundColor: '#f5f3ed'}}>
+          <h1 style={{color: '#1f1d1d', fontFamily: 'gleffy'}}>Loading...</h1>
         </main>
       )}
     </div>
