2691dfd56622f129b0fa37a5909d43e9951b18e4
Joon-Z <39546939+Joon-Z@users.noreply.github.com>
2024-12-21 10:44:09 -0800

    route to thankyou page after submitting message

--- app/contact/page.js
+++ app/contact/page.js
@@ -1,50 +1,59 @@
 "use client";
 
 import NavNoAnimation from "../components/navNoAnimation";
-import {useRouter} from 'next/router';
+import { useRouter } from "next/navigation";
 
 // import styles from '../style/Contact.module.css';
 
 export default function Contact() {
     const router = useRouter();
-    
+
+    function navToThankYou() {
+        router.push('/thankyou');
+    }
+
     async function handleSubmit(event) {
         event.preventDefault();
         const formData = new FormData(event.target);
 
-        const access_key = process.env.NEXT_PUBLIC_WEB3FORM_KEY; // For Jason: create a .env.local file for environment variables
+        const access_key = process.env.NEXT_PUBLIC_WEB3FORM_KEY; // For Jason: create a .env.local file for your web3form access key (also make sure that access key is prepended with NEXT_PUBLIC)
         formData.append("access_key", access_key);
 
         const object = Object.fromEntries(formData);
         const json = JSON.stringify(object);
 
-        const response = await fetch("https://api.web3forms.com/submit", {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-              Accept: "application/json"
-            },
-            body: json
-        });
-        const result = await response.json();
-        if (result.success) {
-            console.log(result);
+        try {
+            const response = await fetch("https://api.web3forms.com/submit", {
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/json",
+                  Accept: "application/json"
+                },
+                body: json
+            });
+            const result = await response.json();
+            if (result.success) {
+                console.log(result);
+                navToThankYou();
+            } else {
+                console.log("Form submission failed: ", result);
+            }
+        } catch (error) {
+            console.log(" Error submitting form: ", error);
         }
     }
 
-    function navToThankYou() {
-        router.push('/thankyou');
-    }
+    
 
   return (
     <>
       {/* <NavNoAnimation/> */}
       <h1 className="call-to-action">Get In Touch</h1>
       <form onSubmit={handleSubmit}>
-        <input type="text" name="name" placeholder="Name"/>
-        <input type="email" name="email" placeholder="Email"/>
-        <input name="message" placeholder="Message"/>
-        <button type="submit" onClick={navToThankYou}>Submit Form</button>
+        <input type="text" name="name" placeholder="Name" required/>
+        <input type="email" name="email" placeholder="Email" required/>
+        <input name="message" placeholder="Message" required/>
+        <button type="submit">Submit Form</button>
       </form>
     </>
   );
--- app/thankyou/page.js
+++ app/thankyou/page.js
@@ -3,6 +3,7 @@ export default function ThankYou() {
         <>
             <h1>Thank You! We will respond as soon as possible!</h1>
             {/* Need a way to redirect to contact page or home page */}
+            <h2>Back to home page</h2>
         </>
     );
 }
